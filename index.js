fdf19b5298c843dfe69f25802e75b8243b1aa719
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-07 11:22:07 +0700

    Head mini lodash done

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var Minilodash = require('./lib/minilodash.js')
 
 
+console.log("isEmpty");
+console.log(`Minilodash.isEmpty([1,2,3]) // `,Minilodash.isEmpty([1,2,3]));
+console.log(`Minilodash.isEmpty([]) // `,Minilodash.isEmpty([])+"\n");
 
-console.log(Minilodash.isEmpty([1,2,3]));
-console.log(Minilodash.isEmpty([]));
+console.log("Count");
+console.log(`Minilodash.count([1,2,3]) // `,Minilodash.count([1,2,3]));
+console.log(`Minilodash.count([]) // `,Minilodash.count([])+"\n");
 
-console.log(Minilodash.count([1,2,3]));
-console.log(Minilodash.count([]));
+console.log("head");
+console.log(`Minilodash.head([1,2,3]) // `,Minilodash.head([1,2,3]));
+console.log(`Minilodash.head([5,2,3]) // `,Minilodash.head([5,2,3])+"\n");
+
+
+console.log("tail");
+console.log(`Minilodash.head([1,2,3]) // `,Minilodash.tail([1,2,3]));
+console.log(`Minilodash.head([5,3,3]) // `,Minilodash.tail([5,3,3]));
+
+
+console.log("\nflatten");
+console.log(`Minilodash.flatten([1,[2,3],4,[5,[6,7]]]) // `,Minilodash.flatten([1,[2,3],4,[5,[6,7]]]));
