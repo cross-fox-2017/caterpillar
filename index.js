4c7031bafb17c9004f5252282fba6c15fff88dc4
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-07 11:11:43 +0700

    index

---
+++ index.js
@@ -0,0 +1,12 @@
+const sanScript = require('./lib/sanScript')
+
+var array_number1 = []
+var array_number2 = ['irsan', 'ganteng', 'kece', 'abis', 'parah']
+
+// isEmpty
+console.log(sanScript.isEmpty(array_number2))
+// count
+console.log(sanScript.count(array_number2))
+
+// tail
+console.log(sanScript.tail(array_number2))
