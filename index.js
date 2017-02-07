38a90da2eaf69b5f0dcff5efb2e8e697327447b7
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-07 11:08:38 +0700

    count Done

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ var Minilodash = require('./lib/minilodash.js')
 
 console.log(Minilodash.isEmpty([1,2,3]));
 console.log(Minilodash.isEmpty([]));
+
+console.log(Minilodash.count([1,2,3]));
+console.log(Minilodash.count([]));
