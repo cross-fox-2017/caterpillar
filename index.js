f7813cc1e6d0ea75ededb683734502eea7db8ca0
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-07 11:22:21 +0700

    tail mini lodash done

--- index.js
+++ index.js
@@ -17,7 +17,3 @@ console.log(`Minilodash.head([5,2,3]) // `,Minilodash.head([5,2,3])+"\n");
 console.log("tail");
 console.log(`Minilodash.head([1,2,3]) // `,Minilodash.tail([1,2,3]));
 console.log(`Minilodash.head([5,3,3]) // `,Minilodash.tail([5,3,3]));
-
-
-console.log("\nflatten");
-console.log(`Minilodash.flatten([1,[2,3],4,[5,[6,7]]]) // `,Minilodash.flatten([1,[2,3],4,[5,[6,7]]]));
