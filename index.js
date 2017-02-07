83e6b55749e0095c0d5680b09825d3caff749d60
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-07 12:15:43 +0700

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const sanScript = require('./lib/sanScript')
 
 var array_number1 = []
 var array_number2 = ['irsan', 'ganteng', 'kece', 'abis', 'parah']
+var array_number3 = ['irsan', ['ganteng', 'kece'], 'banget', ['tapi', ['boong', 'gitu']]]
 
 // isEmpty
 console.log(sanScript.isEmpty(array_number2))
@@ -10,3 +11,7 @@ console.log(sanScript.count(array_number2))
 
 // tail
 console.log(sanScript.tail(array_number2))
+// head
+
+// flatten
+console.log(sanScript.flatten(array_number3))
